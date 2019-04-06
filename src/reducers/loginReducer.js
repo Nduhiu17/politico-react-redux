25185408c61c1ded815da0112ce38ef4b67a1a8a
nduhiu17 <nduhiu254@gmail.com>
2019-04-06 22:28:27 +0300

    feature(login) user should be able to login -  create login reducer

---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,22 @@
+
+
+
+const initialState = {
+    loginDetails: [],
+}
+
+
+export default function (state = initialState,action) {
+    switch (action.type) {
+        case LOGIN_USER:
+            return {
+                ...state,
+                loginDetails: action.payload
+            };
+
+        default:
+            return state
+
+    }
+
+}
