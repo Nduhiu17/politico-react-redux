252afbf0c6c7a7d090191656d9aed360fcc5eb47
nduhiu17 <nduhiu254@gmail.com>
2019-04-07 15:53:01 +0300

    feature(register)user should be able to register for an account - create register page with its components - connect the page with redux

---
+++ src/components/register/registerFooter.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class RegisterFooter extends Component {
+    render() {
+        return (
+            <div>
+                <div className="footer">
+                    <p>&copy; 2019 Politico</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RegisterFooter;
